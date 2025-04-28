666741acecdd1e5bc31ffb44419f8b31fedaf621
victorcauss1 <vikosbb08@gmail.com>
2025-04-28 11:00:16 +0200

    Update test ia.py
    autre essai de module ia

--- test ia.py
+++ test ia.py
@@ -20,17 +20,27 @@
 
 # print ("Texte extrait de l'image :{texte_extrait}")
 
-import easyocr
+# import easyocr
 
-reader = easyocr.Reader(['fr'])
+# reader = easyocr.Reader(['fr'])
 
-# Lire une image
-result = reader.readtext('image_avec_aliments.jpg')
+# # Lire une image
+# result = reader.readtext('image_avec_aliments.jpg')
 
-# Liste des aliments que tu veux détecter
-aliments = ["pain", "tomate", "pomme", "riz", "fromage", ...]
+# # Liste des aliments que tu veux détecter
+# aliments = ["pain", "tomate", "pomme", "riz", "fromage", ...]
 
-# Filtrer les résultats pour ne garder que les mots d'aliments
-for (bbox, text, prob) in result:
-    if any(aliment.lower() in text.lower() for aliment in aliments):
-        print("Aliment trouvé:", text)
+# # Filtrer les résultats pour ne garder que les mots d'aliments
+# for (bbox, text, prob) in result:
+#     if any(aliment.lower() in text.lower() for aliment in aliments):
+#         print("Aliment trouvé:", text)
+from PIL import Image
+
+# Ouvrir une image
+image = Image.open('file:///H:/Documents/Nsi/ticket_de_caisse.jpg')
+
+# Appliquer des modifications à l'image (conversion en noir et blanc par exemple)
+bw_image = image.convert('L')
+
+# Sauvegarder l'image modifiée
+bw_image.save('image_bw.jpg')
