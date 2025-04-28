ad3d8ea1687867e6dbe95797580b747eee3730d9
taki-winie <cyprien.carlot@icloud.com>
2025-04-28 10:49:04 +0200

    Update test ia.py

--- test ia.py
+++ test ia.py
@@ -3,7 +3,7 @@ import pytesseract
 
 # Si tesseract n'est pas dans le PATH, vous devez spécifier son chemin
 # Exemple : pour Windows, le chemin peut être quelque chose comme "C:/Program Files/Tesseract-OCR/tesseract.exe"
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"  # Mettez le chemin correct ici
+# pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"  # Mettez le chemin correct ici
 
 def extraire_texte(image_path):
     # Ouvrir l'image
@@ -15,7 +15,7 @@ def extraire_texte(image_path):
     return texte
 
 # Exemple d'utilisation
-image_path = 'chemin/vers/votre/image.png'  # Remplacez par le chemin de votre image
+image_path = 'ticket.png'  # Remplacez par le chemin de votre image
 texte_extrait = extraire_texte(image_path)
 
 print("Texte extrait de l'image :")
