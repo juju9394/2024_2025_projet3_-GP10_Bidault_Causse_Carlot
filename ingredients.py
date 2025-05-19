9462b95e006cdbd49eb52e6ef569aa91222f61cf
taki-winie <cyprien.carlot@icloud.com>
2025-05-19 11:04:28 +0200

    Create ingredients.py

---
+++ ingredients.py
@@ -0,0 +1,46 @@
+ingredients={
+    "fruits": [
+        "pomme", "banane", "orange", "fraise", "raisin", "kiwi", "poire", "abricot", 
+        "prune", "mangue", "ananas", "melon", "pêche", "nectarine", "citrons", "framboise", 
+        "myrtille", "figue", "papaye", "litchi", "clementine", "grenade", "rhubarbe"
+    ],
+    "legumes": [
+        "carotte", "pomme de terre", "tomate", "oignon", "ail", "courgette", "poivron", 
+        "concombre", "aubergine", "haricot vert", "brocoli", "chou-fleur", "épinard", 
+        "salade", "laitue", "radis", "betterave", "céleri", "artichaut", "poireau", "chou", 
+        "navet", "petit pois", "fenouil", "asperge", "patate douce", "topinambour"
+    ],
+    "feculents": [
+        "pomme de terre", "riz", "pâtes", "semoule", "polenta", "quinoa", "lentilles", 
+        "haricots secs", "pois chiches", "blé", "épeautre", "orge", "farine", "sarrasin", 
+        "amarante", "patate douce", "maïs", "pois cassés"
+    ],
+    "viandes": [
+        "bœuf", "poulet", "dinde", "porc", "agneau", "canard", "lapin", "veau", 
+        "sanglier", "jambon", "saucisse", "bacon", "pâté", "charcuterie", "magret de canard",
+        "côte de porc", "escalope de poulet", "filet de bœuf", "côtelettes d'agneau"
+    ],
+    "poissons_et_fruits_de_mer": [
+        "saumon", "truite", "thon", "morue", "sardine", "maquereau", "merlu", "raie", 
+        "anguille", "sole", "bar", "langouste", "crevette", "huître", "moule", "palourde", 
+        "coquille Saint-Jacques", "crabe", "calamar", "pieuvre"
+    ], 
+    "produits_laitiers": [
+        "lait", "fromage", "yaourt", "crème", "beurre", "fromage blanc", "fromage de chèvre", 
+        "camembert", "brie", "emmental", "gruyère", "mozzarella", "feta", "chèvre frais", 
+        "beurre doux", "beurre salé", "crème fraîche"
+    ],
+    "boissons": [
+        "eau", "jus de fruits", "vin", "bière", "thé", "café", "cacao"
+        
+    ],
+    "sucreries_et_confiseries": [
+        "chocolat", "bonbons", "biscuit", "glace", 
+        "caramel", "miel", "sucre", "compote de pommes", "confiture", "madeleine"
+    ],
+    "épices_et_condiments": [
+        "sel", "poivre", "paprika", "curcuma", "curry", "thym", "romarin", "basilic", 
+        "ciboulette", "persil", "menthe", "safran", "gingembre", "coriandre", "moutarde", 
+        "vinaigre", "huile d'olive", "huile de tournesol", "sauce soja", "ketchup", "mayonnaise"
+    ]
+}
